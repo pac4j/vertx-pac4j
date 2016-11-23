b9e7589826788dc5d46c595e3fed52f2fea394ae
Jeremy Prime <jeremy.prime@millross-consultants.com>
2016-11-23 18:01:17 +0000

    Make the MultiProfileTest compile-time check for the assertThat method rather than runtime
    We had a run-time check in on this interface to check the superclass. This doesn't really make sense as we could just as easily make the required method part of the interface and then refer to it in our default implementations. If we find a more generic case, then we can have this interface extend another interface which exposes the relevant required method.

--- src/test/kotlin/org/pac4j/vertx/handler/impl/MultiProfileTest.kt
+++ src/test/kotlin/org/pac4j/vertx/handler/impl/MultiProfileTest.kt
@@ -5,7 +5,6 @@ import io.vertx.core.json.JsonObject
 import io.vertx.core.logging.LoggerFactory
 import org.hamcrest.Matcher
 import org.hamcrest.core.Is
-import org.pac4j.vertx.Pac4jAuthHandlerIntegrationTestBase
 import java.util.function.Consumer
 
 /**
@@ -18,23 +17,24 @@ interface MultiProfileTest {
         private val LOG = LoggerFactory.getLogger(MultiProfileTest::class.java)
     }
 
+    /**
+     * Requires assertThat to be exposed by test implementing interface so we can use it. We may move that into its
+     * own interface later if we also create another test interface which uses it, but for now it's reasonable to keep it
+     * here.
+     */
+    fun <T>assertThat(actual: T, matcher: Matcher<T>);
+
     fun validateProfilesInBody(body: Buffer, validations: List<Pair<String, Consumer<JsonObject>>>) {
 
         LOG.info("Validating profiles in " + body)
 
-        fun <T>invokeAssertThat(actual: T, matcher: Matcher<T>) = if (this is Pac4jAuthHandlerIntegrationTestBase) {
-            assertThat(actual, matcher)
-        } else {
-            throw IllegalStateException("Only Pac4hAuthHandlerIntegrationTestBase extenders are permitted to call validateProfilesInBody")
-        }
-
         fun validateJsonObjectChild(jsonObject: JsonObject?, key: String, childValidator: Consumer<JsonObject>) {
-            invokeAssertThat<Any>(jsonObject!!.containsKey(key), Is.`is`(true))
+            assertThat<Any>(jsonObject!!.containsKey(key), Is.`is`(true))
             childValidator.accept(jsonObject.getJsonObject(key))
         }
 
         val bodyAsJson = JsonObject(body.toString())
-        invokeAssertThat(bodyAsJson.size(), Is.`is`(validations.size))
+        assertThat(bodyAsJson.size(), Is.`is`(validations.size))
 
         validations.forEach {
             validateJsonObjectChild(bodyAsJson, it.first, it.second)
